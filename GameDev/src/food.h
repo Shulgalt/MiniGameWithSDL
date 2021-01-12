7660140906ed5f1d8bc88f13980479a23eab25a5
Paper <Paper@DESKTOP-JPJ3IJ8>
2021-01-12 19:00:03 +0300

    Added new class for game

---
+++ GameDev/src/food.h
@@ -0,0 +1,17 @@
+/*
+ * food.h
+ *
+ *  Created on: 12 янв. 2021 г.
+ *      Author: Paper
+ */
+
+#ifndef FOOD_H_
+#define FOOD_H_
+
+class SNAKE_FOOD {
+public:
+	SNAKE_FOOD();
+	virtual ~SNAKE_FOOD();
+};
+
+#endif /* FOOD_H_ */
---
+++ GameDev/src/formula.h
@@ -0,0 +1,17 @@
+/*
+ * formula.h
+ *
+ *  Created on: 12 янв. 2021 г.
+ *      Author: Paper
+ */
+
+#ifndef FORMULA_H_
+#define FORMULA_H_
+
+class SNAKE_FORMULA {
+public:
+	SNAKE_FORMULA();
+	virtual ~SNAKE_FORMULA();
+};
+
+#endif /* FORMULA_H_ */
